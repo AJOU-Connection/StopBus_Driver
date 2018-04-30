c8484e8172b4d18060829bef9be6aa4b1334515f
Hy_K <qu22n96@gmail.com>
2018-04-30 19:30:39 +0900

    erase socket change to httpClient

--- app/src/main/java/com/example/kimheeyeon/testapplication/MainActivity.java
+++ app/src/main/java/com/example/kimheeyeon/testapplication/MainActivity.java
@@ -7,9 +7,7 @@ import android.widget.TextView;
 
 import java.io.*;
 import java.net.URISyntaxException;
-//import java.net.Socket;
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
+
 
 
 //socket 통신 구현해야함
@@ -18,27 +16,6 @@ public class MainActivity extends Activity {
     private BufferedReader mIn;
     private PrintWriter mOut;
 
-//    private Client mSocket = new Client("19",90);
-
-//    private String html = "";
-//    private Handler mHandler;
-//
-//    private Socket socket;
-//
-//    private BufferedReader networkReader;
-//    private BufferedWriter networkWriter;
-//
-//    private String ip = "xxx.xxx.xxx.xxx"; // IP
-//    private int port = 9999; // PORT번호
-
-    private Socket mSocket;
-    {
-        try {
-            mSocket = IO.socket("http://chat.socket.io");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,21 +32,6 @@ public class MainActivity extends Activity {
         Bus BackUpBus       =   new Bus("backup");
         Bus MyBus           =   new Bus("my");
 
-//        try {
-//            Route Route     =   new Route();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            //toss로 txt가 없다고 명시
-//        }
-
-//        try {
-//            mSocket = IO.socket("SERVER URL");
-//            mSocket.connect();
-//        } catch(URISyntaxException e) {
-//            e.printStackTrace();
-//        }
-
-        mSocket.connect();
 
         String ip = "192.168.0.10";
         int port = 5555;
