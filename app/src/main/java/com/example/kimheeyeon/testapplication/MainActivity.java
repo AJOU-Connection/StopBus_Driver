b5c4e143a2fdcd7ea0a206de6918dbafad4391d8
Hy_K <qu22n96@gmail.com>
2018-05-03 19:23:53 +0900

    driver setup page

--- app/src/main/java/com/example/kimheeyeon/testapplication/MainActivity.java
+++ app/src/main/java/com/example/kimheeyeon/testapplication/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.setup_page);
 
 
         MyClass myClass = new MyClass();
