d7ab72652dd83fab91a6bd920dea64b9b1846ba9
Hy_K <qu22n96@gmail.com>
2018-05-14 23:28:04 +0900

    add Second Communication unit

--- app/src/main/java/com/example/kimheeyeon/testapplication/DriverActivity.java
+++ app/src/main/java/com/example/kimheeyeon/testapplication/DriverActivity.java
@@ -3,21 +3,22 @@ package com.example.kimheeyeon.testapplication;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.view.View;
 
 import android.content.Intent;
 
 import java.io.*;
-import java.net.URISyntaxException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import android.content.ContentValues;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DriverActivity extends Activity{
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +49,106 @@ public class DriverActivity extends Activity{
         TextView NextStationDirection = (TextView)findViewById(R.id.NextStationDirection);
         NextStationDirection.setText(SettedBus.getBusInfo().getPath().get(3).getStationName().concat(" 방향"));
 
+//        String url = "http://stop-bus.tk/driver/register";
+//
+//        JSONObject sendData = new JSONObject();
+//        try {
+//            sendData.put("plateNo" , ((TextView) findViewById( R.id.Car_Number )).getText().toString());
+//            sendData.put("routeID" , ((TextView) findViewById( R.id.bus_ID )).getText().toString());
+//            Log.d("sending", sendData.getString("plateNo"));
+//            Log.d("sending", sendData.getString("routeID"));
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+
+//        DriverActivity.ConnectThread thread = new DriverActivity.ConnectThread(url, sendData);
+//        thread.start();
+//
+//        try {
+//            thread.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+    }
+    class ConnectThread extends Thread {
+        String urlStr;
+        JSONObject s_Data;
+
+        public ConnectThread(String inStr, JSONObject map){
+            urlStr = inStr;
+            this.s_Data = map;
+        }
+
+        public void run(){
+
+            try{
+                final String output = request(urlStr, this.s_Data);
+                handler.post(new Runnable(){
+                    @Override
+                    public void run() {
+                        Log.d("gmmm", output);
+                    }
+                });
+
+                jsonParserList(output);
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
+        private void jsonParserList(String pRecvServerPage) {
+
+            Log.i("서버에서 받은 전체 내용 : ", pRecvServerPage);
+
+            try {
+                JSONObject jsonObject = new JSONObject(pRecvServerPage);
+
+                JSONObject jHeader = jsonObject.getJSONObject("header");  // JSONObject 추출
+                Log.d("PARSING", jHeader.getString("result"));
+
+                JSONObject jBody = jsonObject.getJSONObject("body");  // JSONObject 추출
+                //Log.d("PARSING", jBody.getString("frontBus"));
 
 
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
 
+        private String request(String urlStr, JSONObject map){
+            StringBuilder output = new StringBuilder();
 
+            try{
+                URL url = new URL(urlStr);
 
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 
+                if(conn != null){
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
 
+                    if (map != null) { // 웹 서버로 보낼 매개변수가 있는 경우우
+                        OutputStream os = conn.getOutputStream(); // 서버로 보내기 위한 출력 스트림
+                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); // UTF-8로 전송
+                        bw.write(map.toString()); // 매개변수 전송
+                        bw.flush();
+                        bw.close();
+                        os.close();
+                    }
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) { // 연결에 성공한 경우
+                        String line;
+                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); // 서버의 응답을 읽기 위한 입력 스트림
+                        while ((line = br.readLine()) != null) // 서버의 응답을 읽어옴
+                            output.append(line);
+                    }
+                }
+            }catch(Exception ex){
+                Log.e("SampleHttp", "Exception in processing response",ex);
 
+            }
+            return output.toString();
+        }
     }
 }
