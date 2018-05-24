0f88b23ff6c999ae8baf3d69dd4dee1b69f5c657
Hy_K <qu22n96@gmail.com>
2018-05-24 18:55:07 +0900

    add file delete

--- app/src/main/java/com/example/kimheeyeon/testapplication/FileControl.java
+++ app/src/main/java/com/example/kimheeyeon/testapplication/FileControl.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -36,11 +37,45 @@ public class FileControl {
         return text.toString();
     }
 
+    public boolean DeleteFile(File file){
+        boolean result;
+        if(file!=null&&file.exists()){
+            file.delete();
+            result = true;
+        }else{
+            result = false;
+        }
+        return result;
+    }
+
 //    public void WriteFile(String fileName, String data) throws Exception {
-//        FileOutputStream os = openFileOutput("makingTest.txt", MODE_PRIVATE);
+//        FileOutputStream os = openFileOutput(fileName, MODE_PRIVATE);
 //        os.write(data.getBytes());
 //        os.close();
 //    }
+
+    public boolean WriteFile(File file , byte[] file_content){
+        boolean result;
+        FileOutputStream fos;
+        if(file!=null&&file.exists()&&file_content!=null){
+            try {
+                fos = new FileOutputStream(file);
+                try {
+                    fos.write(file_content);
+                    fos.flush();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            result = true;
+        }else{
+            result = false;
+        }
+        return result;
+    }
 
     public File CreateFile(File dir , String file_path){
         File file = null;
