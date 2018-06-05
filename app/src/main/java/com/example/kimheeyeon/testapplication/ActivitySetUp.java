adbbbd9ebf57e3f141bfa0246f983e738783b6f5
Hy_K <qu22n96@gmail.com>
2018-06-05 16:28:36 +0900

    init with err

--- app/src/main/java/com/example/kimheeyeon/testapplication/ActivitySetUp.java
+++ app/src/main/java/com/example/kimheeyeon/testapplication/ActivitySetUp.java
@@ -28,27 +28,34 @@ import org.json.JSONObject;
 public class ActivitySetUp extends Activity {
     Handler handler = new Handler();
     Bus settedBus = new Bus();
-    String busID = "";
-    String carNumber = "";
+    String busID = ""; // 20번
+    String carNumber = ""; //경기...
+    String routeId = ""; //234000024
     //private ArrayList<String> locationList = new ArrayList<String>();
     String locationList[];
+
+    String busAreaList[];
+    String busRouteIdList[];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setup_page2);
+        setContentView(R.layout.setup_page_local);
 
         final ProgressBar P_Bar = (ProgressBar) findViewById(R.id.progressBar);
         P_Bar.setVisibility(View.INVISIBLE); //To set visible
 
         final Button Confirm_Button = (Button) findViewById(R.id.Confirm_Button);
-        Button search = (Button) findViewById(R.id.Search);
+        Button Area_Search = (Button) findViewById(R.id.Area_Search);
+        final Button bus_search = (Button) findViewById(R.id.bus_search);
+
 
         Confirm_Button.setOnClickListener(
             new Button.OnClickListener() {
                 public void onClick(View v) {
-                    TextView bus_id = (TextView) findViewById(R.id.bus_ID);
-                    busID = bus_id.getText().toString();
+//                    TextView bus_id = (TextView) findViewById(R.id.bus_ID);
+//                    busID = bus_id.getText().toString();
 
                     P_Bar.setVisibility(View.VISIBLE);
 
@@ -56,7 +63,7 @@ public class ActivitySetUp extends Activity {
                     //파일이 있으면 가지고와서 정리(bus 데이터 만든다는 뜻)
                     //없으면 서버로부터 받아오기 + txt file로 저장
                     File testfile = ActivitySetUp.this.getFilesDir();
-                    File file = new File(testfile, busID.concat(".txt"));
+                    File file = new File(testfile, routeId.concat(".txt"));
 
                     FileControl fc = new FileControl();
 
@@ -82,7 +89,7 @@ public class ActivitySetUp extends Activity {
                         JSONObject sendData = new JSONObject();
                         try {
                             sendData.put("plateNo", "경기00가1234");
-                            sendData.put("routeID", busID);
+                            sendData.put("routeID", routeId);
                             Log.d("sending", sendData.getString("plateNo"));
                             Log.d("sending", sendData.getString("routeID"));
                         } catch (JSONException e) {
@@ -105,7 +112,7 @@ public class ActivitySetUp extends Activity {
                             try {
                                 Intent intent = new Intent(ActivitySetUp.this, ActivityDriver.class);
 
-                                Log.d("ack data", settedBus.getBusInfo().getBusNumber());
+                                //Log.d("ack data", settedBus.getBusInfo().getBusNumber());
                                 intent.putExtra("busData", settedBus);
 
                                 startActivity(intent);
@@ -118,11 +125,9 @@ public class ActivitySetUp extends Activity {
             }
         );
 
-        search.setOnClickListener(
+        bus_search.setOnClickListener(
             new Button.OnClickListener() {
                 public void onClick(View v) {
-                    TextView bus_id = (TextView) findViewById(R.id.bus_ID);
-                    busID = bus_id.getText().toString();
 
                     P_Bar.setVisibility(View.VISIBLE);
 
@@ -131,7 +136,7 @@ public class ActivitySetUp extends Activity {
 
                     JSONObject sendData = new JSONObject();
                     try {
-                        sendData.put("routeID", busID);
+                        sendData.put("routeID", routeId);
                         Log.d("sending", sendData.getString("routeID"));
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -171,6 +176,61 @@ public class ActivitySetUp extends Activity {
                 }
             }
         );
+
+        Area_Search.setOnClickListener(
+            new Button.OnClickListener(){
+                public void onClick(View v){
+                    TextView bus_id = (TextView) findViewById(R.id.bus_ID);
+                    busID = bus_id.getText().toString();
+
+                    String url = "http://stop-bus.tk/user/search?type=route";
+                    String version = "areaList";
+
+                    JSONObject sendData = new JSONObject();
+                    try {
+                        sendData.put("keyword", busID);
+                        Log.d("sending", sendData.getString("keyword"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    ConnectThread Area_thread = new ConnectThread(url, sendData, version);
+                    Area_thread.start();
+
+                    try {
+                        Area_thread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    Spinner Car_Area = (Spinner) findViewById(R.id.Car_Area);
+
+                    Car_Area.setVisibility(View.VISIBLE);
+                    bus_search.setVisibility(View.VISIBLE);
+
+                    ArrayAdapter<String> spinnerArray = new ArrayAdapter<String>(ActivitySetUp.this, android.R.layout.simple_spinner_item, busAreaList);
+                    Car_Area.setAdapter(spinnerArray);
+
+                    Car_Area.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String item = (String)parent.getSelectedItem();
+                            for( int i = 0 ; i < busAreaList.length; i++){
+                                if(busAreaList[i].compareTo(item) == 0)
+                                    routeId = busRouteIdList[i];
+                            }
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+
+                }
+            }
+        );
     }
 
     class ConnectThread extends Thread {
@@ -202,6 +262,8 @@ public class ActivitySetUp extends Activity {
                     case "busList" :
                         parseBusLocation(output);
                         break;
+                    case "areaList" :
+                        parseAreaList(output);
                     default:
                         System.out.println("setup is fail");
                 }
@@ -272,7 +334,7 @@ public class ActivitySetUp extends Activity {
                     System.out.print("nul,,?");
                 }
                 settedBus.setBusInfo(jBody);
-                settedBus.setVehicleNumber(busID);
+                settedBus.setVehicleNumber(routeId);
                 settedBus.setCarNumber(carNumber);
             } else {
                 Log.d("GET DATA ERR", "FAIL TO GET BUSLIST");
@@ -311,15 +373,62 @@ public class ActivitySetUp extends Activity {
                     for(int i = 0; i < BusList.length(); i++){
                         JSONObject binfo = BusList.getJSONObject(i);
                         Log.d("Compare", binfo.getString("plateNo").concat(" and ").concat(binfo.getString("stationSeq")));
-                        //BusLocation nLocation = new BusLocation(binfo.getString("plateNo"), Integer.parseInt(binfo.getString("stationSeq")));
-                        //this.locationList.add(this.locationList.size() , binfo.getString("plateNo"));
                         this.locationList[i] = binfo.getString("plateNo");
                     }
-
-                    System.out.println("shit");
                 }
             }else{
                 Log.d("fail to find","in Driver Activity AT BUSLOCATION");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    private void parseAreaList(String pRecvServerPage) {
+
+        Log.i("서버에서 받은 전체 내용 : ", pRecvServerPage);
+
+        try {
+            JSONObject jsonObject = new JSONObject(pRecvServerPage);
+
+            JSONObject jHeader = jsonObject.getJSONObject("header");  // JSONObject 추출
+            Log.d("PARSING", jHeader.getString("result"));
+
+            if(jHeader.getString("result").compareTo("true") == 0) {
+
+                JSONArray testarray = jsonObject.getJSONArray("body");
+                if(testarray==null){
+                    System.out.print("json is null when get areaList");
+                    return;
+                }
+
+                if (jHeader.getString("result").compareTo("true") != 0)
+                    System.out.println("errrr!!1");
+                else{
+                    JSONArray AreaList = jsonObject.getJSONArray("body");
+                    //강제로 null시켜서 비우기
+
+                    int count = 0;
+                    for(int i = 0; i < AreaList.length(); i++){
+                        JSONObject binfo = AreaList.getJSONObject(i);
+                        if(binfo.getString("routeNumber").compareTo(busID) == 0)
+                            count++;
+                    }
+
+                    this.busAreaList = new String[count];
+                    this.busRouteIdList = new String[count];
+
+                    int number = 0;
+
+                    for(int i = 0; i < AreaList.length(); i++){
+                        JSONObject binfo = AreaList.getJSONObject(i);
+                        if(binfo.getString("routeNumber").compareTo(busID) == 0) {
+                            this.busRouteIdList[number] = binfo.getString("routeID");
+                            this.busAreaList[number++] = binfo.getString("regionName");
+                        }
+                    }
+                }
+            }else{
+                Log.d("fail to find","in Driver Activity AT AREAINFO");
             }
         } catch (JSONException e) {
             e.printStackTrace();
